d5ec30a43bea124c99294442550a0e3fd9829332
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-22 11:49:15 +0530

    roboshop

--- kubeconfig_jenkins.sh
+++ kubeconfig_jenkins.sh
@@ -1,5 +1,6 @@
 install kubernetes plugins in the Jenkins
     1) Kubernetes Continuous Deploy (A Jenkins plugin to deploy resource configurations to a Kubernetes cluster)
+        Note: Latest plugin is not working so install old one d:/DevOps/jenkins/
     2) Kubernetes plugin (this will integrate jenkins and kubenetes)
 
 execute kubectl config view in kubernetes cluster
@@ -7,5 +8,7 @@ execute kubectl config view in kubernetes cluster
 
 go to jenkins manage Jenkins select Manage Nodes and Clouds
     select configure clouds
-    add kuberbetes 
-    
+    add kuberbetes config file in credentials (.kube/config)
+
+kubectl create namespace name to create namespace for an app
+kubectl get namespaces to get/list all namespaces
